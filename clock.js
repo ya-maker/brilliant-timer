3770613ff781e15754f9bb318a7cb32e46b62d1c
안정인 <ji5489@gmail.com>
2019-11-27 23:26:47 +0900

    Fix clock offset & fix clock border
    - Implement clock offset
    - Changed clock border behaviour

--- clock.js
+++ clock.js
@@ -13,18 +13,18 @@ window.location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi,
 const clockCanvas = document.getElementById("clockface");
 const context = clockCanvas.getContext("2d");
 const ClockFaceOffset = Object.freeze({
-  "TWELVE": { x: 0, y: -7, deg: 0 },
   "ONE":    { x: 5, y: -2, deg: 30 },
-  "TWO":    { x: 5, y: -2, deg: 60 },
-  "THREE":  { x: 7, y: 0, deg: 90 },
-  "FOUR":   { x: 5, y: 2, deg: 120 },
-  "FIVE":   { x: 5, y: 2, deg: 150 },
-  "SIX":    { x: 0, y: 7, deg: 180 },
-  "SEVEN":  { x: -5, y: 2, deg: 210 },
-  "EIGHT":  { x: -5, y: 2, deg: 240 },
-  "NINE":   { x: -7, y: 0, deg: 270 },
-  "TEN":    { x: -5, y: -2, deg: 300 },
-  "ELEVEN": { x: -5, y: -2, deg: 330 }
+  "TWO":    { x: 7, y: -2, deg: 60 },
+  "THREE":  { x: 9, y: 3, deg: 90 },
+  "FOUR":   { x: 9, y: 7, deg: 120 },
+  "FIVE":   { x: 5, y: 11, deg: 150 },
+  "SIX":    { x: 0, y: 13, deg: 180 },
+  "SEVEN":  { x: -5, y: 11, deg: 210 },
+  "EIGHT":  { x: -9, y: 10, deg: 240 },
+  "NINE":   { x: -9, y: 4, deg: 270 },
+  "TEN":    { x: -7, y: -2, deg: 300 },
+  "ELEVEN": { x: -5, y: -2, deg: 330 },
+  "TWELVE": { x: 0, y: -4, deg: 0 }
 });
 
 /**
@@ -33,33 +33,45 @@ const ClockFaceOffset = Object.freeze({
 
 /**
  * Draws red clock foreground.
- * 
+ * @param {HTMLElement} canvas Canvas to get width and height
  * @param {CanvasRenderingContext2D} context Context to draw into
  * @param {Number} centerLocX Clock center X position
  * @param {Number} centerLocY Clock center Y position
  * @param {Number} radius Clock radius
  * @param {Number} arc_degree Clock arc degree
  */
-function drawRedClockForeground(context, centerLocX, centerLocY, radius, arc_degree) {
+function drawRedClockForeground(canvas, context, centerLocX, centerLocY, radius, arc_degree) {
   let beginDegrees = 0
   let endDegrees = arc_degree
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
   context.fillStyle = "red";
   context.beginPath();
   
   // Begins from center
-  context.moveTo(centerLocX, centerLocY);
+  if(endDegrees != 360)
+    context.moveTo(centerLocX, centerLocY);
 
   // Center point -> Arc -> Arc Endpoint
   context.arc(centerLocX, centerLocY, radius, (beginDegrees - 90) / 180 * Math.PI, (endDegrees - 90) / 180 * Math.PI, false);
   context.fill();
   
   // Arc Endpoint -> Center
-  context.lineTo(centerLocX, centerLocY);
+  if(endDegrees != 360)
+    context.lineTo(centerLocX, centerLocY);
   context.stroke();
 }
 
-
+/**
+ * Draws clock face into given clock time location.
+ * @param {CanvasRenderingContext2D} context Context to draw into
+ * @param {Number} centerLocX Clock center X position
+ * @param {Number} centerLocY Clock center Y position
+ * @param {Number} radius Clock radius
+ * @param {Number} direction Clock radius
+ * @param {String} text Text to draw into
+ */
 function drawClockFace(context, centerLocX, centerLocY, radius, direction, text) {
   let faceOffset = ClockFaceOffset[direction];
   let clockSpitPosition = getClockspitLocation(centerLocX, centerLocY, radius, faceOffset.deg);
@@ -75,7 +87,13 @@ function drawClockFace(context, centerLocX, centerLocY, radius, direction, text)
   );
 }
 
-
+/**
+ * Get clock spit position on given degree.
+ * @param {Number} centerLocX Clock center X position
+ * @param {Number} centerLocY Clock center Y position
+ * @param {Number} radius Clock radius
+ * @param {Number} degree Clock spit location degree
+ */
 function getClockspitLocation(centerLocX, centerLocY, radius, degree) {
   return {
     x: centerLocX + Math.cos((degree - 90) * Math.PI / 180) * radius,
@@ -96,20 +114,27 @@ const ClockInfo = Object.freeze({
 });
 
 let clockTime = parseInt(params.time);
+let clockTimeEachSpit = clockTime / 12;
 
 drawRedClockForeground(
+  clockCanvas,
   context,
   ClockInfo.Center.X,
   ClockInfo.Center.Y,
   ClockInfo.Radius,
-  200
+  360
 );
 
-drawClockFace(
-  context,
-  ClockInfo.Center.X,
-  ClockInfo.Center.Y,
-  ClockInfo.Radius,
-  "TWELVE",
-  "12"
-);
+let clockSpitText = clockTimeEachSpit;
+Object.keys(ClockFaceOffset).forEach((offsetName, offsetIndex) => {
+  drawClockFace(
+    context,
+    ClockInfo.Center.X,
+    ClockInfo.Center.Y,
+    ClockInfo.Radius,
+    offsetName,
+    clockSpitText.toString()
+  );
+
+  clockSpitText += clockTimeEachSpit;
+});
